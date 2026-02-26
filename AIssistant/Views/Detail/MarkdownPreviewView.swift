92b4f837d9898e513f8dd3b21d988626fc026965
Ben Gottlieb <ben@standalone.com>
2026-02-26 07:39:38 -0600

    added markdown support and an icon

--- AIssistant/Views/Detail/MarkdownPreviewView.swift
+++ AIssistant/Views/Detail/MarkdownPreviewView.swift
@@ -6,23 +6,17 @@
 //
 
 import SwiftUI
+import MarkdownUI
 
 struct MarkdownPreviewView: View {
 	let markdown: String
 
 	var body: some View {
 		ScrollView {
-			if let attributed = try? AttributedString(markdown: markdown, options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)) {
-				Text(attributed)
-					.textSelection(.enabled)
-					.frame(maxWidth: .infinity, alignment: .leading)
-					.padding()
-			} else {
-				Text(markdown)
-					.textSelection(.enabled)
-					.frame(maxWidth: .infinity, alignment: .leading)
-					.padding()
-			}
+			Markdown(markdown)
+				.textSelection(.enabled)
+				.frame(maxWidth: .infinity, alignment: .leading)
+				.padding()
 		}
 	}
 }
